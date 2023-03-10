86dc02e2c11219c2cf1e6ac46bf02c67e97a429c
jihwan.jeong <wnaks5945@gmail.com>
2023-03-10 11:39:56 +0900

    align updates

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -31,63 +31,43 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
+      appBar: AppBar(
+        title: const Text("Test Title"),
+      ),
+      body: Container(
+        child: Stack(
           children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height / 3,
-              child: SingleChildScrollView(
-                  child: Column(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 300,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 300,
-                    color: Colors.purple,
-                  ),
-                ],
-              )),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              color: Colors.black26,
             ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height / 3,
-              child: SingleChildScrollView(
-                  child: Column(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 300,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 300,
-                    color: Colors.orange,
-                  ),
-                ],
-              )),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 150,
+                color: Colors.brown,
+              ),
             ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height / 3,
-              child: SingleChildScrollView(
-                  child: Column(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 300,
-                    color: Colors.green,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 300,
-                    color: Colors.pink,
-                  ),
-                ],
-              )),
-            )
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                margin: const EdgeInsets.only(left: 40, bottom: 150),
+                width: 100,
+                height: 100,
+                color: Colors.amber,
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                margin: const EdgeInsets.only(bottom: 200),
+                width: 200,
+                height: 70,
+                color: Colors.lightBlue[200],
+              ),
+            ),
           ],
         ),
       ),
